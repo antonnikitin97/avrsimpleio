3501437e5e2df3f978184cad60d1f6c6667f41ff
Anton Nikitin <antonnikitin@live.co.uk>
2016-11-14 14:28:08 +0000

    Added a general intro comment at the top of line Begun coding setPinInput function

--- simpleio/simpleio/library.c
+++ simpleio/simpleio/library.c
@@ -1,47 +1,51 @@
 /*
- * simpleio.c
+ * A wrapper library made for the AVR micrcontroller simplifying intefacing with the avr/io library
+ * In making this full awareness is given to the existence of such a library already but it is hoped that any programmer using this
+ * will find it a lot easier to work with the GPIO and hence be able to focus more on their programming.
  *
- * Created: 12/11/2016 17:20:23
- * Author : Anton
+ * Anton.
  */ 
 
 #include <avr/io.h>
 
-
 /* Replace with your library code */
 
 void setPortInput(char port){
-	switch(port){
-		case 'A':
+	switch(convertToLower(port)){
 		case 'a':
 			DDRA = 0x00;
 			break;
-		case 'B':
 		case 'b':
 			DDRA = 0x00;
 			break;
-		case 'C':
 		case 'c':
 			DDRA = 0x00;
 			break;
 	}
 }
 
-void setPINSInRestOut(char port, uint8_t PINToSetIN[]){
-	
+void setPinInput(char port, uint8_t pinNumber){
+    switch(convertToLower(port)){
+        case 'a':
+            DDRA &= ~(1 << pinNumber);
+            break;
+        case 'b':
+            DDRB &= ~(1 << pinNumber);
+            break;
+        case 'c':
+            DDRC &= ~(1 << pinNumber);
+            break;
+    }
 }
 
 void enableAllPullUpsPort(char port){
-	switch(port){
-		case 'A':
+	switch(convertToLower(port)){
 		case 'a':
 			PORTA = 0xFF;
 			break;
-		case 'B':
 		case 'b':
 			PORTB = 0xFF;
 			break;
-		case 'C':
 		case 'c':
 			PORTC = 0xFF;
 			break;
@@ -49,20 +53,23 @@ void enableAllPullUpsPort(char port){
 }
 
 void enableAllTriStatePort(char port){
-	switch(port){
-		case 'A':
+	switch(convertToLower(port)){
 		case 'a':
 			PORTA = 0x00;
 			break;
-		case 'B':
 		case 'b':
 			PORTB = 0x00;
 			break;
-		case 'C':
 		case 'c':
 			PORTC = 0x00;
 			break;
 	}
 }
 
-
+char convertToLower(char input){
+    if(input >= 97 && input <= 122) {
+        return input;
+    }else{
+        return input + 32;
+    }
+}
